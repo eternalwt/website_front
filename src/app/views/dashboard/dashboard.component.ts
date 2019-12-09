1754d715cd895dca3bacc164cf9ab4e4cf209792
eternalwt <eternalwt@qq.om>
2019-12-09 19:10:42 +0800

    first echarts line chart

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -14,10 +14,14 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     this.options = {
       title: {
-        text: '身高'
+        text: '折线图'
       },
       legend: {
-        type: 'plain'
+        // type: 'plain',
+        type: 'scroll',
+        data: [],
+        top: '0',
+        left: 'center'
       },
       tooltip: {
         trigger: 'axis'
@@ -28,17 +32,29 @@ export class DashboardComponent implements OnInit {
         bottom: '3%',
         containLabel: true
       },
-      // xAxis: {
-      //   type: 'category',
-      //   boundaryGap: false,
-      //   data: ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18']
-      // },
-      // yAxis: {
-      //   type: 'value',
-      //   min: 70
-      // }
-      
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18']
+      },
+      yAxis: {
+        type: 'value',
+        series: []
+      },
+      series: []
     };
+
+    let value = [[], []];
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < 17; j++) {
+        value[i].push(Math.random().toFixed(2));
+      }
+    }
+
+    for (let i = 0; i < 2; i++) {
+        this.options.legend.data.push(i.toString());
+        this.options.series.push({name: i.toString(), type:"line", smooth: true, data: value[i]});
+    }
   }
 
 }
