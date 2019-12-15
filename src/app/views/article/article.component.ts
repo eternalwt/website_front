d47cfe4787e012e7805d06059318b13d23ad040e
eternalwt <eternalwt@qq.om>
2019-12-15 22:51:50 +0800

    add article

--- src/app/views/article/article.component.ts
+++ src/app/views/article/article.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms'
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { ChangeEvent } from '@ckeditor/ckeditor5-angular';
+import { DataService } from 'src/app/service/data.service';
 // import * as DocumentEditor from '@ckeditor/ckeditor5-angular';
 
 
@@ -12,13 +13,27 @@ import { ChangeEvent } from '@ckeditor/ckeditor5-angular';
 })
 export class ArticleComponent implements OnInit {
 
-  public Editor = ClassicEditor;
+  public Editor = ClassicEditor;//ckeditor
+  config = {};//ckeditor配置
 
-  config = {};
+  title: string = "";//标题
+  content: string = '';//正文
+  submitted = false;
+  success = false;
 
-  constructor() { }
+  messageForm: FormGroup;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private dataService: DataService
+  ) { }
 
   ngOnInit() {
+    // this.messageForm = this.formBuilder.group({
+    //   title: ['', Validators.required], // 标题
+    //   content: ['', Validators.required] // 正文
+    // });
   }
 
   onReady(editor) {
@@ -28,11 +43,32 @@ export class ArticleComponent implements OnInit {
     // );
   }
 
-  content = '';
   onChange({editor}: ChangeEvent) {
     // const data = editor.getData();
     // console.log(data);
     // this.content = data;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // if (this.messageForm.invalid) {
+    //     return;
+    // }
+
+    // this.dataService.addArticle(this.messageForm.value).subscribe(res => {
+    this.dataService.addArticle({"title": this.title, "content": this.content}).subscribe(res => {
+      debugger;
+      // todo 给提示
+      // else {
+      //   // todo 提示用户名密码错误、显示验证码等（Angular Material 2）
+      // }
+    
+    });
+
+    // todo 把使用formBuilder的写法写通
+
+    this.success = true;
   }
 
 }
