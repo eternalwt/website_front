47c2d80d64e197c761512020a85e60d2b5e6c6c0
eternalwt <eternalwt@qq.om>
2020-03-04 23:31:13 +0800

    add pie chart

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -7,12 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  options: any;
+  lineChartOptions: any;// 折线图数据
+  pieChartOptions: any; // 饼图数据
 
   constructor() { }
 
   ngOnInit() {
-    this.options = {
+    this.lineChartOptions = {
       title: {
         text: '折线图'
       },
@@ -56,9 +57,31 @@ export class DashboardComponent implements OnInit {
     value[1][5] = null;
 
     for (let i = 0; i < 2; i++) {
-        this.options.legend.data.push(i.toString());
-        this.options.series.push({name: i.toString(), type:"line", smooth: true, data: value[i]});
+        this.lineChartOptions.legend.data.push(i.toString());
+        this.lineChartOptions.series.push({name: i.toString(), type:"line", smooth: true, data: value[i]});
     }
+
+
+    // 饼图
+    this.pieChartOptions = {
+      series : [
+          {
+              name: '访问来源',
+              type: 'pie',    // 设置图表类型为饼图
+              radius: '55%',  // 饼图的半径，外半径为可视区尺寸（容器高宽中较小一项）的 55% 长度。
+              data:[          // 数据数组，name 为数据项名称，value 为数据项值
+                  {value:235, name:'视频广告'},
+                  {value:274, name:'联盟广告'},
+                  {value:310, name:'邮件营销'},
+                  {value:335, name:'直接访问'},
+                  {value:400, name:'搜索引擎'}
+              ]
+          }
+      ]
+    };
+
+
+
   }
 
 }
