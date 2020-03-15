f160ff1f1913770d35e67a8512fa5690dd4ce5c5
eternalwt <eternalwt@qq.om>
2020-03-15 20:12:35 +0800

    add msg center

---
+++ src/app/views/msg/msg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-msg',
+  templateUrl: './msg.component.html',
+  styleUrls: ['./msg.component.less']
+})
+export class MsgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
